111f51d34d1850c4621881a7a18ac92e8c669664
odinellefsen <odin@ellefsen.fo>
2026-03-22 22:39:07 +0000

    Fix edge case handling in climb_stairs function by initializing variable for n=1

--- climbing_stairs.rs
+++ climbing_stairs.rs
@@ -2,7 +2,11 @@ fn main() {
     struct Solution;
     impl Solution {
         pub fn climb_stairs(n: i32) -> i32 {
-            let x = n - 1;
+            let mut x = 0;
+            if (n - 1 == 0) {
+                x = 1;
+            }
+
             let mut y = 0;
             if (n - 2 >= 0) {
                 y = n - 2;
