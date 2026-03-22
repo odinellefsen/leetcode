6e4a1d231cb2e8629d5ec33c05abb20a3acf220a
odinellefsen <odin@ellefsen.fo>
2026-03-22 22:28:41 +0000

    Add variables for previous steps in climb_stairs function

--- climbing_stairs.rs
+++ climbing_stairs.rs
@@ -2,6 +2,8 @@ fn main() {
     struct Solution;
     impl Solution {
         pub fn climb_stairs(n: i32) -> i32 {
+            let x = n - 1;
+            let y = n - 2;
             0
         }
     }
