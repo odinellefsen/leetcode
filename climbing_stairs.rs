cb0538513313a2965914ae3f1591aca89752074b
odinellefsen <odin@ellefsen.fo>
2026-03-22 22:06:02 +0000

    Enhance climbing_stairs implementation with a loop to print step indices

--- climbing_stairs.rs
+++ climbing_stairs.rs
@@ -1,5 +1,13 @@
 fn main() {
+    struct Solution;
     impl Solution {
-        pub fn climb_stairs(n: i32) -> i32 {}
+        pub fn climb_stairs(n: i32) -> i32 {
+            for i in 0..n {
+                println!("{}", i);
+            }
+            0
+        }
     }
+
+    Solution::climb_stairs(10);
 }
