9bb73383ae623f45de4d32d7e0f4daf62cacb2e1
odinellefsen <odin@ellefsen.fo>
2026-03-22 22:41:31 +0000

    Update main function to print result for n=2 and adjust variable initialization in climb_stairs function

--- climbing_stairs.rs
+++ climbing_stairs.rs
@@ -5,15 +5,19 @@ fn main() {
             let mut x = 0;
             if (n - 1 == 0) {
                 x = 1;
+            } else {
+                x = n - 1;
             }
 
             let mut y = 0;
             if (n - 2 >= 0) {
+                y = n - 2;
+            } else {
                 y = n - 2;
             }
             x + y
         }
     }
 
-    println!("{}", Solution::climb_stairs(1));
+    println!("{}", Solution::climb_stairs(2));
 }
