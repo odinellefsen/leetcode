5c9f5319097812243a7348c99b8486f368658253
odinellefsen <odin@ellefsen.fo>
2026-03-22 22:27:31 +0000

    Remove debug loop from climb_stairs function in climbing_stairs.rs

--- climbing_stairs.rs
+++ climbing_stairs.rs
@@ -2,9 +2,6 @@ fn main() {
     struct Solution;
     impl Solution {
         pub fn climb_stairs(n: i32) -> i32 {
-            for i in 0..n {
-                println!("{}", i);
-            }
             0
         }
     }
