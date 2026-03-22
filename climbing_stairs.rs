e50ab99a5d1e3b8919411f37086e70b4a8e0599e
odinellefsen <odin@ellefsen.fo>
2026-03-22 22:46:30 +0000

    Refactor climb_stairs function to directly return results for n=1 and n=2, simplifying the logic for larger values

--- climbing_stairs.rs
+++ climbing_stairs.rs
@@ -2,23 +2,14 @@ fn main() {
     struct Solution;
     impl Solution {
         pub fn climb_stairs(n: i32) -> i32 {
-            let mut x = 0;
-
-            if (n - 1 == 0) {
-                x = 1;
-            } else {
-                x = n - 1;
+            if (n == 1) {
+                return 1;
+            }
+            if (n == 2) {
+                return 2;
             }
 
-            let mut y = 0;
-
-            if (n - 2 >= 0) {
-                y = n - 2;
-            } else {
-                y = n - 2;
-            }
-
-            x + y
+            n - 1 + n - 2
         }
     }
 
