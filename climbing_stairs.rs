9f233a995830f569ae39626bbc4033556a8f64fc
odinellefsen <odin@ellefsen.fo>
2026-03-22 22:35:00 +0000

    Update climb_stairs function to handle edge cases and modify main to print result for n=1

--- climbing_stairs.rs
+++ climbing_stairs.rs
@@ -2,9 +2,14 @@ fn main() {
     struct Solution;
     impl Solution {
         pub fn climb_stairs(n: i32) -> i32 {
-            n - 1 + n - 2
+            let x = n - 1;
+            let mut y = 0;
+            if (n - 2 >= 0) {
+                y = n - 2;
+            }
+            x + y
         }
     }
 
-    println!("{}", Solution::climb_stairs(4));
+    println!("{}", Solution::climb_stairs(1));
 }
