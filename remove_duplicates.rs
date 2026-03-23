50d6accc4a893211f06b8ae7ff5672a82f7a4bfa
odinellefsen <odin@ellefsen.fo>
2026-03-23 21:49:08 +0000

    Add remove_duplicates.rs file and update ListNode definition with corrected formatting and comments

--- remove_duplicates.rs
+++ remove_duplicates.rs
@@ -1,19 +1,16 @@
 fn main() {
-    Definition for singly-linked list.
+    // Definition for singly-linked list.
     #[derive(PartialEq, Eq, Clone, Debug)]
     pub struct ListNode {
-      pub val: i32,
-      pub next: Option<Box<ListNode>>
+        pub val: i32,
+        pub next: Option<Box<ListNode>>,
     }
-    
+
     impl ListNode {
-      #[inline]
-      fn new(val: i32) -> Self {
-        ListNode {
-          next: None,
-          val
+        #[inline]
+        fn new(val: i32) -> Self {
+            ListNode { next: None, val }
         }
-      }
     }
     struct Solution;
     impl Solution {
