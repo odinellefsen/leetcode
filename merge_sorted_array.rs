e6edc2fc8977c26efaea4ac240c35202fa375da2
odinellefsen <odin@ellefsen.fo>
2026-03-24 22:00:31 +0000

    Update debug print statements in merge function to use debug formatting for better readability

--- merge_sorted_array.rs
+++ merge_sorted_array.rs
@@ -2,8 +2,8 @@ fn main() {
     struct Solution;
     impl Solution {
         pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-            println!("nums1: {}", nums1);
-            println!("nums2: {}", nums2);
+            println!("nums1: {:?}", nums1);
+            println!("nums2: {:?}", nums2);
         }
     }
 }
