fa160e32858807943a178fe7dc7a0f3fd6945fae
odinellefsen <odin@ellefsen.fo>
2026-03-24 21:58:56 +0000

    Add debug print statements to merge function in merge_sorted_array.rs for input tracing

--- merge_sorted_array.rs
+++ merge_sorted_array.rs
@@ -1,6 +1,9 @@
 fn main() {
     struct Solution;
     impl Solution {
-        pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {}
+        pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+            println!("nums1: {}", nums1);
+            println!("nums2: {}", nums2);
+        }
     }
 }
