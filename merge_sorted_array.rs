68878a9c59d3ac8518a908ee4f04f4c0b98c96d7
odinellefsen <odin@ellefsen.fo>
2026-03-24 22:10:36 +0000

    Update debug print statements in merge function to include sizes of input vectors for enhanced clarity

--- merge_sorted_array.rs
+++ merge_sorted_array.rs
@@ -1,15 +1,15 @@
-fn main() {
-    struct Solution;
-    impl Solution {
-        pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-            println!("nums1: {:?}", nums1);
-            println!("nums2: {:?}", nums2);
-        }
-    }
+struct Solution;
 
+impl Solution {
+    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+        println!("m = {m}, n = {n}");
+        println!("nums1 = {:?}, nums2 = {:?}", nums1, nums2);
+    }
+}
+
+fn main() {
     let mut nums1 = vec![1, 3, 5, 7];
-    let m = 3;
     let mut nums2 = vec![2, 4, 6];
-    let n = 3;
-    Solution::merge(&mut nums1, m, &mut nums2, n);
+
+    Solution::merge(&mut nums1, 4, &mut nums2, 3);
 }
