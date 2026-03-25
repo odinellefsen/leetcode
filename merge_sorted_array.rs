faccbdb4229d1d8a18fec502b0b0b43d4a60f50a
odinellefsen <odin@ellefsen.fo>
2026-03-25 22:28:10 +0000

    Remove unnecessary debug print statements in merge function of merge_sorted_array.rs to clean up output while maintaining functionality

--- merge_sorted_array.rs
+++ merge_sorted_array.rs
@@ -2,11 +2,10 @@ struct Solution;
 
 impl Solution {
     pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-        println!("m = {m}, n = {n}");
-        println!("nums1 = {:?}, nums2 = {:?}", nums1, nums2);
         let mut i: usize = 0;
         let mut j: usize = 0;
         let mut arr: Vec<i32> = vec![67, 69];
+
         while i < nums1.len() {
             println!("{}", nums1[i]);
             arr.push(nums1[i]);
@@ -17,6 +16,7 @@ impl Solution {
             j = 0;
             i += 1;
         }
+
         println!("{:?}", arr)
     }
 }
