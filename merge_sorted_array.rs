f2541ff0567a5526460dd6a5c65912bc543c2d23
odinellefsen <odin@ellefsen.fo>
2026-03-29 22:41:57 +0100

    Add debug output for merged array in main function.

--- merge_sorted_array.rs
+++ merge_sorted_array.rs
@@ -25,6 +25,7 @@ fn main() {
     let mut nums2 = vec![2, 5, 6];
 
     Solution::merge(&mut nums1, 3, &mut nums2, 3);
+    println!("{:?}", nums1);
     assert_eq!(nums1, vec![1, 2, 2, 3, 5, 6]);
 
     let mut nums1 = vec![0];
