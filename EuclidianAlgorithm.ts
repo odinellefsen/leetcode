e1defd051cb58b0a89e68f9ca39fac7630b9e9d7
odinellefsen <odin@ellefsen.fo>
2026-04-26 17:02:41 +0100

    Comment out test values and console logs in EuclidianAlgorithm to clean up the code and focus on the GCD function implementation.

--- EuclidianAlgorithm.ts
+++ EuclidianAlgorithm.ts
@@ -23,10 +23,10 @@ function gcd(a: number, b: number): number {
 //const d = gcd(24140, 16762);
 //console.log(`The Greatest Common Divisior is: ${d}`);
 
-const a = 24140;
-const b = 16762;
-const quotient = Math.floor(a / b);
-const remainder = a - (b * quotient);
-console.log(`quotient: ${quotient}`);
-console.log(`remainder ${remainder}`);
-console.log(`dividend: ${quotient * b + remainder}`);
+// const a = 68;
+// const b = 34;
+// const quotient = Math.floor(a / b);
+// const remainder = a - (b * quotient);
+// console.log(`quotient: ${quotient}`);
+// console.log(`remainder ${remainder}`);
+// console.log(`dividend: ${quotient * b + remainder}`);
