37346e41adf3f28c2d32d1616c5d5458027a6686
odinellefsen <odin@ellefsen.fo>
2026-06-17 15:05:24 +0100

    refactor: streamline isPalindrome function by removing unnecessary checks and improving readability

--- isPalindrome_3.js
+++ isPalindrome_3.js
@@ -5,21 +5,32 @@
  */
 var isPalindrome = function(x) {
     if (x < 0) {
-        return false;
+        return false
     }
 
     let reversed_num = 0;
     let x_x = x;
     let loop = true;
+    const x_str = String(x);
+    let i = x_str.length - 1;
     while (loop) {
         const last_digit = x_x % 10;
         const new_x = Math.floor(x_x / 10);
         x_x = new_x;
         reversed_num = reversed_num * 10 + last_digit;
+        const current_num = Math.floor(x / Math.pow(10, i));
 
-        if (new_x === 0) {
+        if (current_num !== reversed_num) {
+            return false;
+        }
+
+        if (x_x === 0) {
             loop = false;
         }
+
+        
+
+        i--;
     }
     if (x === reversed_num) {
         return true
