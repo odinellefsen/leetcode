2974eb9b820c0d867fc52f8d0934f84d21770fdd
odinellefsen <odin@ellefsen.fo>
2026-06-17 01:44:03 +0100

    refactor: remove debug logs from two-sum solution for cleaner code

--- twoSum_2.js
+++ twoSum_2.js
@@ -7,19 +7,9 @@ var twoSum = function(nums, target) {
     const map = new Map();
     for (let i = 0; i < nums.length; i++) {
         const current_value = nums[i];
-        console.log("current_value: ", current_value);
         const difference = target - current_value;
-        console.log("difference: ", difference);
-        const keyExists = map.has(current_value);
-        
-        if (keyExists && current_value * 2 === target) {
-            console.log("Oof")
-            const index = map.get(current_value)
-            return [i, index]
-        }
 
         const diff_get = map.get(difference)
-        console.log("diff_get: ", diff_get)
 
         if (diff_get !== undefined) {
             return [i, diff_get]
