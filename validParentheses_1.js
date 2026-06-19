9ecf77383b521963c24bcf4fdd6b0a5acfe154c5
odinellefsen <odin@ellefsen.fo>
2026-06-19 15:43:16 +0100

    refactor: add debugging console logs in isValid function to trace map state during parentheses validation

--- validParentheses_1.js
+++ validParentheses_1.js
@@ -18,15 +18,26 @@ var isValid = function(s) {
         if (s[i] === ")" && map.get("(") >= 1) {
             map["("] = map.get("(") - 1;
         } else {
+            for (const [key, value] of map) {
+                console.log(key, value);
+            }
             return false;
         }
         if (s[i] === "]" && map.get("[") >= 1) {
             map["["] = map.get("[") - 1
         } else {
+            for (const [key, value] of map) {
+                console.log(key, value);
+            }
             return false;
         }
-
-
+    }
+    
+    for (const [key, value] of map) {
+        console.log(key, value);
     }
 };
 
+const result = isValid("()[]{}");
+
+// console.log("result: ", result)
