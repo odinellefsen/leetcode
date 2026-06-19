f1240a5572240d0b9dbecfbc047357c9a87299d1
odinellefsen <odin@ellefsen.fo>
2026-06-19 12:13:02 +0100

    refactor: remove debugging console logs and improve condition for identifying the shortest word in longestCommonPrefix function

--- longestCommonPrefix_1.js
+++ longestCommonPrefix_1.js
@@ -3,20 +3,15 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    for (const value of strs) {
-        console.log(value)
-    }
-
-    console.log(strs[0] > strs[1]);
-
     let shortest_word = ""
     for (const current_word of strs) {
-        if (current_word < shortest_word || shortest_word.length === 0) {
-            console.log("new shortest word: ", current_word)
+        if (current_word.length < shortest_word.length || shortest_word.length === 0) {
             shortest_word = current_word;
         }
     }
-    console.log("shortest word:", shortest_word)
+    
+    
+    
 };
 
 const result = longestCommonPrefix(["flower","flow","flight"])
