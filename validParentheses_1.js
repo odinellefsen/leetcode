3504d015042a7da0ec37466f3b58cd5404a00d60
odinellefsen <odin@ellefsen.fo>
2026-06-19 23:57:20 +0100

    bug: was using wrong data structure for problem

--- validParentheses_1.js
+++ validParentheses_1.js
@@ -3,44 +3,9 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    const map = new Map();
+    const stack = [];
 
-    for (let i = 0; i < s.length; i++) {
-
-        if (s[i] === "(") {
-            map.set("(",  map.get("(") ?? 0 + 1);
-        }
-        if (s[i] === "[") {
-            map.set("[", map.get("[") ?? 0 + 1);
-        }
-        if (s[i] === "{") {
-            map.set("{", map.get("{") ?? 0 + 1);
-        }
-        
-        if (s[i] === ")" && map.get("(") >= 1) {
-            map.set("(", map.get("(") - 1);
-        } else if (s[i] === ")") {
-            return false;
-        }
-        if (s[i] === "]" && map.get("[") >= 1) {
-            map.set("[", map.get("[") - 1);
-        } else if (s[i] === "]") {
-            return false;
-        }
-        if (s[i] === "}" && map.get("{") >= 1) {
-            map.set("{", map.get("{") - 1);
-        } else if (s[i] === "}") {
-            return false;
-        } 
-    }
-
-    for (const [key, value] of map) {
-        if (value !== 0) {
-            return false;
-        }
-    }
-
-    return true;
+    
 };
 
 const result = isValid("()[]{}");
