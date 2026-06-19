ace038a193d75634b8849b031ba5ec2b5b3df5c6
odinellefsen <odin@ellefsen.fo>
2026-06-19 11:53:42 +0100

    feat: implement logic to find the longest common prefix by identifying the shortest word in the array with added console logs for debugging

--- longestCommonPrefix_1.js
+++ longestCommonPrefix_1.js
@@ -3,9 +3,22 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    
+    for (const value of strs) {
+        console.log(value)
+    }
+
+    console.log(strs[0] > strs[1]);
+
+    let shortest_word = ""
+    for (const current_word of strs) {
+        if (current_word < shortest_word || shortest_word.length === 0) {
+            console.log("new shortest word: ", current_word)
+            shortest_word = current_word;
+        }
+    }
+    console.log("shortest word:", shortest_word)
 };
 
 const result = longestCommonPrefix(["flower","flow","flight"])
 
-console.log("result");
+// console.log("result: ", result);
