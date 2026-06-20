79a4c93a75135c507d2bdb6c3e7e1fe7ab4ac6e5
odinellefsen <odin@ellefsen.fo>
2026-06-20 12:40:39 +0100

    fix: correct closing bracket checks in isValid function and improve stack handling logic

--- validParentheses_1.js
+++ validParentheses_1.js
@@ -6,7 +6,38 @@ var isValid = function(s) {
     const stack = [];
 
     for (const char of s) {
-        console.log("char: ", char);
+        console.log("stack: ", stack);
+        if (char === "(" || char === "[" || char === "{") {
+            stack.push(char);
+        }
+
+        if (char === ")") {
+            if (char[s.length - 1] === "(") {
+                stack.pop()
+            } else {
+                return false;
+            }
+        }
+        if (char === "]") {
+            if (char[s.length - 1] === "[") {
+                stack.pop()
+            } else {
+                return false;
+            }
+        }
+        if (char === "}") {
+            if (char[s.length - 1] === "{") {
+                stack.pop()
+            } else {
+                return false;
+            }
+        }
+    }
+
+    if (stack.length === 0) {
+        return true;
+    } else {
+        return false;
     }
 };
 
